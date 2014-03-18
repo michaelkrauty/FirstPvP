73d88823055b06d9f406eb5ec3b9110f1589bb62
Michael Krautkramer <michaelkrauty@gmail.com>
2014-03-18 17:41:59 -0500

    modified:   css/index.css 	modified:   css/shop.css 	new file:   inc/header.php 	modified:   index.php 	modified:   shop.php

---
+++ inc/header.php
@@ -0,0 +1,35 @@
+<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+  <div class='container'>
+    <div class='navbar-header'>
+      <span class='sr-only'>Toggle navigation</span>
+      <span class='icon-bar'></span>
+      <span class='icon-bar'></span>
+      <span class='icon-bar'></span>
+      <a class='navbar-brand' href='index.php';  "vertical-align:middle";> FirstPvP </a>
+    </div>
+    <div class='navbar-collapse'>
+      <ul class='nav navbar-nav'>
+      	<?php echo "<li";if($pageName == "index"){echo" class='active'";}echo">";echo"<a href='index.php' title='Home'>Home</a></li>";?>
+        <?php echo "<li";if($pageName == "shop"){echo" class='active'";}echo">";echo"<a href='shop.php' title='Shop'>Shop</a></li>";?>
+        <?php echo "<li";if($pageName == "wiki"){echo" class='active'";}echo">";echo"<a href='http://google.com' title='Wiki'>Wiki<br></a></li>";?>
+     </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+	  			<a href="" class="dropdown-toggle" data-toggle="dropdown"></span> <?php echo $pageName; ?> <b class="caret"></b></a>
+          <ul class="dropdown-menu">
+            <li><a href="/mpcp/settings/"><span class="glyphicon glyphicon-dashboard"></span> Settings</a></li>
+            <li><a href=""><span class="glyphicon glyphicon-plus-sign"></span> Sub Users</a></li>
+            <li><a href="/mpcp/forum/"><span class="glyphicon glyphicon-question-sign"></span> Support</a></li>
+            <li><a href="/mpcp/knowledgebase/"><span class="glyphicon glyphicon-info-sign"></span> Knowledgebase</a></li>
+            <li><a href="/mpcp/forum/"><span class="glyphicon glyphicon-comment"></span> Forum</a></li>
+            <li><a href=""><span class="glyphicon glyphicon-exclamation-sign"></span> Access Log</a></li>
+            <li class="divider"></li>
+            <li><a href="/mpcp/includes/logout.php"><span class="glyphicon glyphicon-off"></span> Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
+
+
--- shop.php
+++ shop.php
@@ -7,6 +7,7 @@
 		?>
 	</head>
 	<body>
+	<?php include_once "inc/header.php";?>
 		<div class="body">
 		
 		
