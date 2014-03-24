2038785864d4078e7156f2d307fd8751b7f53c37
Michael Krautkramer <michaelkrauty@gmail.com>
2014-03-23 19:51:53 -0500

    update

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 <?php echo $_SESSION["username"];?>
 <?php
 function screenOnline(){
-	return strpos(shell_exec("screen -list mc/"), "firstpvp");
+	if(strpos(shell_exec("screen -list mc/"), "firstpvp")){return true;}else{return false;}
 }
 
 function executeCommand($command){
@@ -10,11 +10,19 @@ function executeCommand($command){
 		shell_exec("screen -x mc/firstpvp -p 0 -X stuff \"`printf \"$command\r\"`\";");
 	}
 }
-
+echo "screenOnline()<br>";
 var_dump(screenOnline());
+echo "shell_exec('screen -list mc/')<br>";
 var_dump(shell_exec("screen -list mc/"));
+echo "executeCommand('say test')<br>";
+var_dump(executeCommand("say test"));
+echo "shell_exec(stuff)";
+var_dump(shell_exec("screen -x mc/firstpvp -p 0 -X stuff \"`printf \"$command\r\"`\";"));
+echo "shell_exec('pwd')<br>";
 var_dump(shell_exec("pwd"));
+echo "_SESSION<br>";
 var_dump($_SESSION);
+echo "_SERVER<br>";
 var_dump($_SERVER);
 
 
